9071df78b312f65b502c48c3ddba24ef11549c87
rajathirumal <rajathirumal98@gmail.com>
2023-07-07 22:01:42 +0530

    Reverting the scan_files() as Python uses Global Interpreter Lock making sure that only one thread can execute - Multithreading reduces the speed of iteration

--- classes/GCodePreprocessing_Utils.py
+++ classes/GCodePreprocessing_Utils.py
@@ -8,14 +8,51 @@ class GCodePreprocessingUtils:
     def __init__(self, source: str) -> None:
         self.sourcefolder = source
 
-    def scan_files(self, scanFilesUnder: Union[str, None]):
+    # def scan_files(self, scanFilesUnder: Union[str, None]):
+    #     """
+    #     Scans the files in the given folder or gets the folder name to scan from the class constructor
+        
+    #     Makes use of the available CPU cores
+
+    #     total size of the files in the folder, returns a 3-tuple followed by the list of full path of the files
+    #     `(size_in_kb, size_in_mb, size_in_gb)`, `[filepath]`
+    #     """
+    #     if scanFilesUnder is None:
+    #         scanFilesUnder = self.sourcefolder
+
+    #     source_file_list = []
+    #     size_in_bytes = 0
+
+    #     with futures.ThreadPoolExecutor() as executor:
+    #         future_to_path = {
+    #             executor.submit(self._scan_folder, folder, scanFilesUnder): folder
+    #             for folder in tqdm(
+    #                 os.listdir(scanFilesUnder),
+    #                 desc=f"Scanning folder: {scanFilesUnder}",
+    #             )
+    #         }
+
+    #         for future in tqdm(
+    #             futures.as_completed(future_to_path), desc="Scanning files"
+    #         ):
+    #             folder = future_to_path[future]
+    #             try:
+    #                 result = future.result()
+    #                 size_in_bytes += result[0]
+    #                 source_file_list.extend(result[1])
+    #             except Exception as exc:
+    #                 print(f"Exception occurred while scanning folder {folder}: {exc}")
+
+    #     sizeinfo = self._calculate_file_size(size_in_bytes)
+
+    #     return sizeinfo, source_file_list
+
+
+    def scan_files(self, scanFilesUnder=None):
         """
         Scans the files in the given folder or gets the folder name to scan from the class constructor
-        
-        Makes use of the available CPU cores
-
         total size of the files in the folder, returns a 3-tuple followed by the list of full path of the files
-        `(size_in_kb, size_in_mb, size_in_gb)`, `[filepath]`
+             `(size_in_kb, size_in_mb, size_in_gb)`, `[filepath]`
         """
         if scanFilesUnder is None:
             scanFilesUnder = self.sourcefolder
@@ -23,27 +60,16 @@ class GCodePreprocessingUtils:
         source_file_list = []
         size_in_bytes = 0
 
-        with futures.ThreadPoolExecutor(max_workers=10) as executor:
-            future_to_path = {
-                executor.submit(self._scan_folder, folder, scanFilesUnder): folder
-                for folder in tqdm(
-                    os.listdir(scanFilesUnder),
-                    desc=f"Scanning folder: {scanFilesUnder}",
-                )
-            }
+        for root, dirs, files in tqdm(os.walk(scanFilesUnder)):
+            for f in files:
+                #  Get the py file full path
+                full_path = os.path.join(root, f)
+                # Get the size of individual files
+                size_in_bytes += os.path.getsize(full_path)
+                # Add file full path to a list
+                source_file_list.append(full_path)
 
-            for future in tqdm(
-                futures.as_completed(future_to_path), desc="Scanning files"
-            ):
-                folder = future_to_path[future]
-                try:
-                    result = future.result()
-                    size_in_bytes += result[0]
-                    source_file_list.extend(result[1])
-                except Exception as exc:
-                    print(f"Exception occurred while scanning folder {folder}: {exc}")
-
-        sizeinfo = self._calculate_file_size(size_in_bytes)
+        sizeinfo = self._clculate_file_size(size_in_bytes)
 
         return sizeinfo, source_file_list
 
